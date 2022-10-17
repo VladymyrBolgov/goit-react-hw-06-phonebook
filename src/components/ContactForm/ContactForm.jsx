82f4ed24fc5e57cff72f809648c375264e3058fb
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-10-17 23:27:28 +0300

    cop

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { nanoid } from 'nanoid';
+import { Formik, Field } from 'formik';
+import {FormBox, FormLabel, FormInput, FormButton,} from './ContactForm.styled';
+import PropTypes from 'prop-types';
+
+const ContactForm = ({ onSubmit }) => {
+  const inputNameId = nanoid();
+  const inputTelId = nanoid();
+
+  const initualValues = {
+    name: '',
+    number: '',
+  };
+
+  const handleOnSubmit = (values, { resetForm }) => {
+    onSubmit(values);
+    resetForm();
+  };
+
+  return (
+    <Formik initialValues={initualValues} onSubmit={handleOnSubmit}>
+      <FormBox>
+        <FormLabel htmlFor={inputNameId}>Name</FormLabel>
+        <Field
+          id={inputNameId}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          as={FormInput}
+          required
+        />
+        <FormLabel htmlFor={inputTelId}>Number</FormLabel>
+        <Field
+          id={inputTelId}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          as={FormInput}
+          required
+        />
+        <FormButton type="submit">Add contact</FormButton>
+      </FormBox>
+    </Formik>
+  );
+};
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default ContactForm;
+
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import {ContactListBox, ContactListItem, ContactListBtn, ContactListText,} from './ContactList.styled';
+
+const ContactList = ({ contacts, deleteOnClick }) => {
+  return (
+    <ContactListBox>
+      {contacts.map(({ id, name, number }) => (
+        <ContactListItem key={id}>
+          <ContactListText>Name: {name}</ContactListText>
+          <ContactListText>Number: {number}</ContactListText>
+          <ContactListBtn
+            type="button"
+            onClick={() => {
+              deleteOnClick(id);
+            }}
+          >
+            Remuve
+          </ContactListBtn>
+        </ContactListItem>
+      ))}
+    </ContactListBox>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  deleteOnClick: PropTypes.func.isRequired,
+};
+
+export default ContactList;
